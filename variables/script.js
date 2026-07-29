94db4e64394bee2b0c5cef18ccf64c8f300fb9a2
jackomosh <jackomosh6@gmail.com>
2026-07-29 20:29:56 +0300

    variable names, const in JS

--- variables/script.js
+++ variables/script.js
@@ -1,7 +1,56 @@
 // a variable is a named storage of data
+// variable names must contain only letters, digits and symbols
+// the first character of a variable name cannot be a digit
+// hyphens are not allowed in variable names
+// variable names are case sensitive
 
 let message;
 
 message = "Hello"
 
 alert(message);
+
+// multiple declaration in one line
+
+let username = "Jack"; let age = 25; let year = 2000;
+
+alert(username);
+alert(age);
+alert(year);
+
+// multiline var naming
+
+let name = "Jack";
+let ages = 25;
+let yob = 2000;
+
+alert(name);
+alert(age);
+alert(yob);
+
+var zone = 1; // the var keyword can also be used in JS
+var zone =2; // change variables values
+alert(zone);
+
+// declare two variables and copy data of one to another
+
+let tutam = "Ruto"
+
+let oneterm;
+
+oneterm = tutam;
+
+alert(oneterm);
+
+// Constants - unchanging variables
+
+const myBirthday = '01.01.2000';
+alert(myBirthday);
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_ORANGE = "#FF7F00";
+const COLOR_BLUE = "#00F";
+
+let color = COLOR_BLUE;
+alert(color);
