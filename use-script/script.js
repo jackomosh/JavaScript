837c8430a365b3cf21133841304386da7298c84c
jackomosh <jackomosh6@gmail.com>
2026-07-30 16:12:21 +0300

    use strict - allows us to catch js errors on fly

---
+++ use-script/script.js
@@ -0,0 +1,8 @@
+"use strict" 
+
+// use script must appear above and ontop only comments can appear above it otherwise it would be inactive
+
+// this code works the modern way
+
+x = 3.14;
+alert(`x is: ${x}`); // Uncaught ReferenceError: x is not defined
