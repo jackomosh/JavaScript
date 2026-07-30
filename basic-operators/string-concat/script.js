12960372ddef065f786b7b31e9c1f18f7c79b462
jackomosh <jackomosh6@gmail.com>
2026-07-30 16:54:26 +0300

    add two strings together

---
+++ basic-operators/string-concat/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let s = "My " + "Name" // concatinates 2 strings
+alert(s);
+
+alert(1 + "2"); // Note that if one operand is a string the other one is converted to a string
+alert('2' + 2); // here also
+
+// Only + operator works that way
+
+alert(6 - '2'); // 4; in subtraction 2 is converted to a number
+alert('6' / '3'); // 2; in division 2 is converted to a number
+
