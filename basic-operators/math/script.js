6edfb374b66a61eaff291019f3dd53daf1d2389b
jackomosh <jackomosh6@gmail.com>
2026-07-30 16:34:47 +0300

    basic modulous/remainder and exponention

---
+++ basic-operators/math/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// an operator is unary if it has a single operand e.g
+
+let x = 1
+x = -x
+alert(x); // 1 operand
+
+let y = 2
+alert(y-x); // Binary - it has two operands y and x
+
+alert(5 % 2); // Answer is 1 - the remainder of 5 divided by 2
+alert(8 % 3); // Answer is 2 - the remainder of 8 divided by 3
+alert(8 % 4); // Answer is 0 - the remainder of 8 divided by 4
+
+// ** Exponention operator raises a to the power of b
+
+alert(2 ** 2);
+alert(2 ** 3);
+
+alert(4 ** 1/2); // Answer is 2 - the power of 1/2 is the same as square root
