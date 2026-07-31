46dbeb0d585fb94a2f93270bf14e2bc89a81f3bc
jackomosh <jackomosh6@gmail.com>
2026-07-31 15:56:53 +0300

    all comparison operators return a boolean value

---
+++ comparison/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+alert(2 > 1); // Should return true
+alert(2==1); // Returns false
+alert(2 != 1); //Returns true
+
+// A comparison result can be assigned to a variable
+
+let result = 5 > 4
+alert(result) // return true
