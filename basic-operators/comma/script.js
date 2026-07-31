ce440d4e158d1cdd566bf1e0f708cc84487e089c
jackomosh <jackomosh6@gmail.com>
2026-07-31 15:26:15 +0300

    the comma operator returns the last expression in a series of expressions

---
+++ basic-operators/comma/script.js
@@ -0,0 +1,8 @@
+'use script'
+
+let a = (1 + 2, 3 + 4)
+alert(a) // 7, since the result is of the last expression only 3+4
+
+// the comma throws everything away except the last expression
+
+// remeber that prompt returns user input as a string
