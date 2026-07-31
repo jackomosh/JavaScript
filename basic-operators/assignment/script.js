40fc16f15394f1e9ef00ed027066ecb5b16d83ec
jackomosh <jackomosh6@gmail.com>
2026-07-31 13:12:31 +0300

    modify assignments using + operator += and *=

--- basic-operators/assignment/script.js
+++ basic-operators/assignment/script.js
@@ -5,3 +5,43 @@
 
 let x = 2 * 2 + 1
 alert(x) // 5
+
+
+// assignment = returns a value
+
+let k = 1
+let l = 2
+
+let m = 3 - (k = l + 1)
+
+alert(k) // 3 since the value of a changed to 3 when assigning c
+alert(m) // 0 since 3-3 is 0
+
+// chaining assignments
+
+let a, b, c;
+
+a = b = c = 2 + 2
+
+alert(a)
+alert(b)
+alert(c)
+
+// its better to split the code into
+// c = 2 + 2
+// b = c
+// a = c
+
+// Modify-in place
+
+let n = 2
+n = n + 5
+n = n * 2
+
+// Now the value of n is 14.
+
+// we can also use it this way
+
+let o = 2
+o += 5
+o *= 2 // the value of o is same
