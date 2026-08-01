1ecd6ae6814341bcdc86dbca78994461a0523cb9
jacomondi <jackomosh6@gmail.com>
2026-08-01 09:39:22 +0300

    comparing using regular equality == and strict equality ===

--- comparison/script.js
+++ comparison/script.js
@@ -15,3 +15,32 @@ alert("z" < "a") // false
 alert("Glow" > "Jacomondi") // false
 alert("Bee" > "Be") // true
 
+
+// comparison of different types
+
+// When comparing values of different types JS converts them values to numbers
+
+alert("2" > 1) // true since "2" is converted to a number
+alert("1" == 1) // true since "1" is now 1
+
+// For boolean values true becomes 1 and false 0
+
+alert(true == 1)
+alert(false == 0)
+
+// strict equality conversion
+
+// Regular equality check ==
+
+// A regular equlity check has a problem, it cannot differentiate 0 from false
+
+// This happens because operands of different types are converted to numbers by the equality operator ==
+alert(0 == false) // true 
+alert("" == false) // true
+
+// Strict equality check ===
+// Checks the equality without type conversion
+
+alert(0 === false) // false since 0 is a number and false is a boolean
+
+// comparison with null and undefined
